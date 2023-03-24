78d32288e000c4289b71647f6d2aa7cdf6fbfa5a
Zane Golas <zane@golas.dev>
2023-03-23 19:46:42 -0700

    MPE Proof Of Concept

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,17 @@ const byte BUTTON_2 = 30;
 const byte LED_GREEN = 33;
 const byte LED_RED = 34;
 const byte LED_BLUE = 35;
+const byte JOYSTICK_1_X = 36;
+const byte JOYSTICK_1_Y = 37;
+const byte JOYSTICK_2_X = 38;
+const byte JOYSTICK_2_Y = 39;
 
 // Sets button 1 and 2 to control MIDI CC 1 and 2
-MIDIswitch mInput1(BUTTON_1, 1, MOMENTARY);
-MIDIswitch mInput2(BUTTON_2, 2, MOMENTARY);
+Bounce mButton1(BUTTON_1, 5);
+Bounce mButton2(BUTTON_2, 5);
+
+int mCurrentJoystick1X = 0;
+int mCurrentJoystick2X = 0;
 
 // Global led variables
 elapsedMillis mElapsedMS = 0;
@@ -28,29 +35,53 @@ void setup() {
     pinMode(LED_BLUE, OUTPUT);
     pinMode(BUTTON_1, INPUT_PULLUP);
     pinMode(BUTTON_2, INPUT_PULLUP);
+    pinMode(JOYSTICK_1_X, INPUT);
+    pinMode(JOYSTICK_1_Y, INPUT);
+    pinMode(JOYSTICK_2_X, INPUT);
+    pinMode(JOYSTICK_2_Y, INPUT);
     digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-    mInput1.send();
-    mInput2.send();
+    mButton1.update();
+    mButton2.update();
 
-    //Green LED for input 1, Red LED for input 2, Pulsing Blue LED for standby
-    digitalWrite(LED_GREEN, mInput1.state);
-    digitalWrite(LED_RED, mInput2.state);
-
-    auto is_writing = (mInput1.state || mInput2.state);
-    if (mElapsedMS > 3 && !is_writing){
-        mElapsedMS = 0;
-        if (mBlueValue >= 255 || mBlueValue <= 0){
-            mIncrement *= -1;
-        }
-        mBlueValue += mIncrement;
-    } else {
-        mBlueValue = 0;
+    // Note On messages when each button is pressed
+    if (mButton1.fallingEdge()) {
+        usbMIDI.sendNoteOn(60, 99, 2);  // 60 = C4
+        digitalWrite(LED_RED, HIGH);
+    }
+    if (mButton2.fallingEdge()) {
+        usbMIDI.sendNoteOn(67, 99, 3);
+        digitalWrite(LED_GREEN, HIGH);
+    }
+    // Note Off messages when each button is released
+    if (mButton1.risingEdge()) {
+        usbMIDI.sendNoteOff(60, 0, 2);  // 60 = C4
+        digitalWrite(LED_RED, LOW);
+    }
+    if (mButton2.risingEdge()) {
+        usbMIDI.sendNoteOff(67, 0, 3);
+        digitalWrite(LED_GREEN, LOW);
     }
 
-    analogWrite(LED_BLUE, mBlueValue);
+
+    int joystick_1_x = analogRead(JOYSTICK_1_X) - 512; // 0 at mid point
+    if (abs(joystick_1_x - mCurrentJoystick1X) > 1)  { // have we moved enough to avoid analog jitter?
+        if (abs(joystick_1_x) > 4) { // are we out of the central dead zone?
+            usbMIDI.sendPitchBend(8 * joystick_1_x, 2); // or -8 depending which way you want to go up and down
+            mCurrentJoystick1X = joystick_1_x;
+        }
+    }
+
+    int joystick_2_x = analogRead(JOYSTICK_2_X) - 512; // 0 at mid point
+    if (abs(joystick_2_x - mCurrentJoystick2X) > 1)  { // have we moved enough to avoid analog jitter?
+        if (abs(joystick_2_x) > 4) { // are we out of the central dead zone?
+            usbMIDI.sendPitchBend(8 * joystick_2_x, 3); // or -8 depending which way you want to go up and down
+            mCurrentJoystick1X = joystick_2_x;
+        }
+    }
+
 
     // This prevents crashes that happen when incoming usbMIDI is ignored.
     while(usbMIDI.read()){}
