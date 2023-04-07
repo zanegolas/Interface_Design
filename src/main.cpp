69fc569a43f4ddfd43622c513515b62567e46344
Zane Golas <zane@golas.dev>
2023-04-07 10:16:14 -0700

    More lidar debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,45 @@
 //2 button midi CC control
 
 #include <Arduino.h>
-#include <usb_midi.h>
-#include <LiquidCrystal_I2C.h>
-#include <rplidar_driver_impl.h>
+#include <RPLidar.h>
 
-const byte LIDAR_MOTOR_PIN = 25; // Lidar TX/RX on 26/27
-char report[80];
+const byte LIDAR_MOTOR_PIN = 2;
 
 // Global Objects
-LiquidCrystal_I2C mLCD(0x27,16,2);
 RPLidar mLidar;
 
 void setup() {
-    mLCD.init();
-    mLCD.noCursor();
-    mLCD.backlight();
-    mLCD.print("Starting Up...");
-
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
 
     pinMode(LIDAR_MOTOR_PIN, OUTPUT);
-    mLidar.begin();
-
-    mLCD.setCursor(0, 1);
-    mLCD.print("Complete!");
-}
-
-void printInfo(){
-    rplidar_response_device_info_t info;
-    mLidar.getDeviceInfo(info);
-
-    snprintf(report, sizeof(report), "model: %d firmware: %d.%d", info.model, info.firmware_version/256, info.firmware_version%256);
-    mLCD.print(report);
-    delay(1000);
+    mLidar.begin(Serial1);
 }
 
 void loop() {
+    if (IS_OK(mLidar.waitPoint())) {
+        float distance = mLidar.getCurrentPoint().distance; //distance value in mm unit
+        float angle    = mLidar.getCurrentPoint().angle; //anglue value in degree
+        bool  startBit = mLidar.getCurrentPoint().startBit; //whether this point is belong to a new scan
+        byte  quality  = mLidar.getCurrentPoint().quality; //quality of the current measurement
 
-    if (!mLidar.isScanning()){
-        mLidar.startScanNormal(false);
-        digitalWrite(LIDAR_MOTOR_PIN, HIGH); // turn on the motor
-        mLCD.clear();
-        mLCD.print("Start Scan...");
-        delay(1000);
+        digitalWrite(LED_BUILTIN, HIGH);
+
+
     } else {
-        mLCD.clear();
-        mLCD.print("Reading...");
-        delay(1000);
-        mLidar.loopScanData();
+        analogWrite(LIDAR_MOTOR_PIN, 0); //stop the rplidar motor
 
-        rplidar_response_measurement_node_hq_t nodes[512];
-        size_t nodeCount = 512; // variable will be set to number of received measurement by reference
-        u_result ans = mLidar.grabScanData(nodes, nodeCount);
+        // try to detect RPLIDAR...
+        rplidar_response_device_info_t info;
+        if (IS_OK(mLidar.getDeviceInfo(info, 100))) {
+            // detected...
+            mLidar.startScan();
 
-        if (IS_OK(ans)) {
-            for (size_t i = 0; i < nodeCount; ++i) {
-                // convert to standard units
-                float angle_in_degrees = static_cast<float>(nodes[i].angle_z_q14) * 90.f / (1 << 14);
-                float distance_in_meters = static_cast<float>(nodes[i].dist_mm_q2) / 1000.f / (1 << 2);
-                mLCD.setCursor(0, 0);
-                mLCD.print(angle_in_degrees);
-                mLCD.setCursor(0, 1);
-                mLCD.print(distance_in_meters);
-            }
+            // start motor rotating at max allowed speed
+            analogWrite(LIDAR_MOTOR_PIN, 255);
+            delay(1000);
         }
+        digitalWrite(LED_BUILTIN, LOW);
     }
-    if (!mLidar.isConnected()) {
-        mLCD.clear();
-        mLCD.print("LIDAR Error");
-    } else {
-        mLCD.clear();
-        printInfo();
-    }
-    // This prevents crashes that happen when incoming usbMIDI is ignored.
-    while(usbMIDI.read()){}
 
 }
