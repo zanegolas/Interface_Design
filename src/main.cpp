f9227fd80e74ca431f565390806e91bb23c541ac
Zane Golas <zane@golas.dev>
2023-04-07 13:01:25 -0700

    Better data speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,59 @@
 //2 button midi CC control
 
 #include <Arduino.h>
-#include <RPLidar.h>
+#include <rplidar_driver_impl.h>
 
 const byte LIDAR_MOTOR_PIN = 2;
 
 // Global Objects
 RPLidar mLidar;
 elapsedMillis mLatency = 0;
+bool mRotated = false;
 int mSampleCount = 0;
-int mQualitySamples = 0;
+char report[80];
 
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
 
     pinMode(LIDAR_MOTOR_PIN, OUTPUT);
-    mLidar.begin(Serial1);
+    mLidar.begin();
+
+    delay(1000);
+    mLidar.startScanExpress(true, RPLIDAR_CONF_SCAN_COMMAND_EXPRESS);
+    digitalWrite(LIDAR_MOTOR_PIN, HIGH); // turn on the motor
+    delay(10);
 
     Serial.begin(9600);
     Serial.println("Setup Complete");
 }
 
 void loop() {
-    if (IS_OK(mLidar.waitPoint())) {
-        float distance = mLidar.getCurrentPoint().distance; //distance value in mm unit
-        float angle    = mLidar.getCurrentPoint().angle; //angle value in degree
-        bool  startBit = mLidar.getCurrentPoint().startBit; //whether this point is belong to a new scan
-        byte  quality  = mLidar.getCurrentPoint().quality; //quality of the current measurement
 
-        mSampleCount++;
+    // loop needs to be send called every loop
+    mLidar.loopScanExpressData();
 
-        if (quality != 0) {
-            mQualitySamples++;
+    // create object to hold received data for processing
+    rplidar_response_measurement_node_hq_t nodes[512];
+    size_t nodeCount = 512; // variable will be set to number of received measurement by reference
+    u_result ans = mLidar.grabScanExpressData(nodes, nodeCount);
+    if (IS_OK(ans)){
+        for (size_t i = 0; i < nodeCount; ++i){
+            // convert to standard units
+            float angle_in_degrees = nodes[i].angle_z_q14 * 90.f / (1<<14);
+            float distance_in_meters = nodes[i].dist_mm_q2 / 1000.f / (1<<2);
+            //Flag Value 1;
+            snprintf(report, sizeof(report), "Degrees: %.2f Distance: %.2f Quality: %d Flag: %d", angle_in_degrees, distance_in_meters, nodes[i].quality, nodes[i].flag);
+            Serial.println(report);
+            mSampleCount++;
         }
-        /* Diagnostic Prints
-        Serial.print("Distance: ");
-        Serial.println(distance);
-        Serial.print("Angle: ");
-        Serial.println(angle);
-        Serial.print("Start Bit: ");
-        Serial.println(startBit);
-        Serial.print("Quality: ");
-        Serial.println(quality); */
-
-        if (startBit) {
-            Serial.print("Start Bit Received After: ");
-            Serial.print(mLatency);
-            Serial.println("ms");
-            Serial.print("Number Of Samples: ");
-            Serial.println(mSampleCount);
-            Serial.print("Quality Samples: ");
-            Serial.println(mQualitySamples);
-            auto samples_per_second = mSampleCount * (1000 / mLatency);
-            Serial.print("Samples Per Second: ");
-            Serial.println(samples_per_second);
-
-            mSampleCount = 0;
-            mQualitySamples = 0;
-            mLatency = 0;
-
-        }
-
-        digitalWrite(LED_BUILTIN, HIGH);
-
-
-    } else {
-        analogWrite(LIDAR_MOTOR_PIN, 0); //stop the rplidar motor
-
-        // try to detect RPLIDAR...
-        rplidar_response_device_info_t info;
-        if (IS_OK(mLidar.getDeviceInfo(info, 100))) {
-            // detected...
-            mLidar.startScan();
-
-            // start motor rotating at max allowed speed
-            analogWrite(LIDAR_MOTOR_PIN, 255);
-            delay(1000);
-        }
-        digitalWrite(LED_BUILTIN, LOW);
     }
 
+//    if (mLatency >= 1000) {
+//        Serial.print("Samples Per Second: ");
+//        Serial.println(mSampleCount);
+//
+//        mSampleCount = 0;
+//        mLatency = 0;
+//    }
 }
